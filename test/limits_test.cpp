671ac1937ce68c2f5d1fc4f8b066d76eecdf1a78
inigogc <inigogc@opendeusto.es>
2020-12-13 22:17:14 +0100

    test: created test parseLimitVec_returnsTrueWithMultipleCorrectLimits

--- test/limits_test.cpp
+++ test/limits_test.cpp
@@ -13,15 +13,19 @@ BOOST_AUTO_TEST_CASE( parseLimitVec_returnsTrueWithCorrectMinLimit ) {
 }
 
 BOOST_AUTO_TEST_CASE( parseLimitVec_returnsTrueWithCorrectMinMaxLimit ) {
-        constexpr unsigned char limitVec[] = {0x01, antiwasm::LIMIT_MIN_MAX, 0x05, 0x08}; //aka [5-8]
+    constexpr unsigned char limitVec[] = {0x01, antiwasm::LIMIT_MIN_MAX, 0x05, 0x08}; //aka [5-8]
 
-        bool isSuccessful = antiwasm::parseLimitVec(const_cast<unsigned char *>(limitVec));
+    bool isSuccessful = antiwasm::parseLimitVec(const_cast<unsigned char *>(limitVec));
 
-        BOOST_ASSERT(isSuccessful);
+    BOOST_ASSERT(isSuccessful);
 }
 
 BOOST_AUTO_TEST_CASE( parseLimitVec_returnsTrueWithMultipleCorrectLimits ) {
+    constexpr unsigned char limitVec[] = {0x01, 0xFF/*incorrect limit type*/, 0x05, 0x08};
 
+    bool isSuccessful = antiwasm::parseLimitVec(const_cast<unsigned char *>(limitVec));
+
+    BOOST_CHECK_EQUAL(isSuccessful, false);
 }
 
 BOOST_AUTO_TEST_CASE( parseLimitVec_returnsFalseWithWronglyFormattedLimits ) {
