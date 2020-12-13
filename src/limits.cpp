209d927d900da82e57f19058fa67b349fd4fb26d
inigogc <inigogc@opendeusto.es>
2020-12-13 15:34:38 +0100

    refactor: performance and style improvements at limits

--- src/limits.cpp
+++ src/limits.cpp
@@ -1,41 +1,41 @@
 #include "../includes/limits.hpp"
 
 namespace antiwasm {
-    void parseLimitVec(unsigned char* limitVec)
+    bool parseLimitVec(unsigned char* limitVec)
     {
         int nLimits = limitVec[0];
         int pointer = 1;
 
-        for(int i = 0; i < nLimits; i++)
+        for(auto i = 0; i < nLimits; i++)
         {
             pointer += getLimits(&limitVec[pointer]);
+            //TODO is bad return false
         }
+        return true;
     }
 
-    int getLimits(unsigned char* limitSection)
+    int getLimits(const unsigned char* limitSection)
     {
         if(limitSection[0] == LIMIT_MIN)
         {
-            int *limits = (int*)malloc(sizeof(int) * 2);
-            limits[0] = limitSection[1];
-            limits[1] = -1;
+            auto limitMin = limitSection[1];
+            //limit max is MAX value
 
-            BOOST_LOG_TRIVIAL(debug) << "Limit[" << limits[0] << "]-[MAX]";
+            BOOST_LOG_TRIVIAL(debug) << "Limit[" << limitMin << "]-[MAX]";
 
             return 2;
         }
         if(limitSection[0] == LIMIT_MIN_MAX)
         {
-            int *limits = (int*)malloc(sizeof(int) * 2);
-            limits[0] = limitSection[1];
-            limits[1] = limitSection[2];
+            auto limitMin = limitSection[1];
+            auto limitMax = limitSection[2];
 
-            BOOST_LOG_TRIVIAL(debug) << "Limit[" << limits[0] << "]-[" << limits[1] << "]";
+            BOOST_LOG_TRIVIAL(debug) << "Limit[" << limitMin << "]-[" << limitMax << "]";
 
             return 3;
         }
         BOOST_LOG_TRIVIAL(error) << "These bytes are not a limit";
-        return 1;   //TODO throw an exception
+        return -1;   //TODO throw an exception
     }
 
 }
