d8dde2a91edfe965f48e1573b251d98dda43e5b4
inigogc <inigogc@opendeusto.es>
2020-12-23 19:21:47 +0100

    feat: added boost::program_options for better arg management

--- includes/init.hpp
+++ includes/init.hpp
@@ -2,6 +2,8 @@
 #define ANTIWASM_INIT_HPP
 
 #include "scanner.hpp"
+#include <iostream>
 #include <boost/log/trivial.hpp>
+#include <boost/program_options.hpp>
 
 #endif //ANTIWASM_INIT_HPP
--- src/init.cpp
+++ src/init.cpp
@@ -1,14 +1,39 @@
 #include "../includes/init.hpp"
 
 int main(int argc, char **argv) {
-    if(argc != 2) {
-        std::cout << "Enter a filename" << std::endl;
-        return -1;
+
+    // Declare the supported options.
+    boost::program_options::options_description argDescription("Allowed options for [my_program]");
+    argDescription.add_options()
+            ("help",
+             "produce this help message")
+            ("filename",
+             boost::program_options::value<std::string>(),
+             "route of the .wasm file");
+
+    boost::program_options::variables_map variablesMap;
+    boost::program_options::store(
+            boost::program_options::parse_command_line(
+                    argc, argv, argDescription), variablesMap);
+    boost::program_options::notify(variablesMap);
+
+    //If one of the options is set to 'help'...
+    if (variablesMap.count("help"))
+    {
+        //Display the options_description
+        std::cout << argDescription << "\n";
     }
 
-    char* classFile = argv[1];
-    BOOST_LOG_TRIVIAL(debug) << "Filename: " << classFile;
+    if (variablesMap.count("filename"))
+    {
+        std::cout
+                << "Compiling " << variablesMap["filename"].as<std::string>() << "'\n";
+        antiwasm::parse( variablesMap["filename"].as<std::string>().c_str() );
+    }
+    else
+    {
+        std::cout
+                << "No filename set\n";
+    }
 
-    antiwasm::parse(classFile);
 }
-
