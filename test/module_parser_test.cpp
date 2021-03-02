b2ca68d7265583e4d1310b7cb347667f2df0d38d
inigogc <inigogc@opendeusto.es>
2021-03-02 21:45:51 +0100

    test: modified tests for module_parser_test

--- test/module_parser_test.cpp
+++ test/module_parser_test.cpp
@@ -1,30 +1,29 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include "../src/module_parser.cpp"
-#include "../src/types_parser.cpp"
 
 BOOST_AUTO_TEST_SUITE( module_parser_test )
 
-BOOST_AUTO_TEST_CASE(parseSections_canParseEverySection)
-{/*
-    std::shared_ptr<Driver> driver = Driver::GetInstance();
-    driver->OpenFile("./files/basic-module.wasm");
-    driver->GetNextBytes(8);
-    auto *firstSectionHeader = driver->GetNextBytes(2);
+BOOST_AUTO_TEST_CASE(parseNextSection_canParseACorrectSection)
+{
+    antiwasm::SectionId sectionId = antiwasm::SectionId::Custom;
+    int sizeOfSection = 1;
+    auto* contentOfSection = static_cast<unsigned char *>(malloc(sizeof(unsigned char) * sizeOfSection)); //Does not matter
 
-    int result = antiwasm::parseSections(firstSectionHeader);
+    auto result = antiwasm::parseNextSection(sectionId, sizeOfSection, contentOfSection);
 
-    BOOST_CHECK_EQUAL(0, result);*/
+    BOOST_CHECK_EQUAL(0, result);
 }
 
-BOOST_AUTO_TEST_CASE(parseSections_failsIfEncountersAnUnknownSection)
-{/*
-    auto *firstHeader = (unsigned char*)malloc(sizeof(unsigned char) * 2);
-    firstHeader[0] = 0xBB; // There's no section with ID = 0xBB
+BOOST_AUTO_TEST_CASE(parseNextSection_failsIfEncountersAnUnknownSection)
+{
+    unsigned char nonExistingSectionId = 100;
+    int sizeOfSection = 1;
+    auto* contentOfSection = static_cast<unsigned char *>(malloc(sizeof(unsigned char) * sizeOfSection)); //Does not matter
 
-    int result = antiwasm::parseSections(firstHeader);
+    auto result = antiwasm::parseNextSection(nonExistingSectionId, sizeOfSection, contentOfSection);
 
-    BOOST_CHECK_EQUAL(1, result);*/
+    BOOST_CHECK_EQUAL(1, result);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
