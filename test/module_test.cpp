d9d287bf65dfb4f62fd5fa06571755ef46d52405
inigogc <inigogc@opendeusto.es>
2021-03-03 14:31:53 +0100

    test: added a new test Module_returnsASectionWithTheRequestedId

--- test/module_test.cpp
+++ test/module_test.cpp
@@ -14,6 +14,16 @@ BOOST_AUTO_TEST_CASE(Module_returnsItsSize)
     BOOST_CHECK_EQUAL(sizeOfModule, returnValue);
 }
 
+BOOST_AUTO_TEST_CASE(Module_returnsASectionWithTheRequestedId)
+{
+    Module module(20);
+    antiwasm::SectionId sectionId = antiwasm::SectionId::Type;
+    Section section(sectionId, 10, nullptr);
 
+    module.addSection(section);
+    auto returnValue = module.getSection(sectionId);
+
+    BOOST_CHECK_EQUAL(sectionId, returnValue.getSectionId());
+}
 
 BOOST_AUTO_TEST_SUITE_END()
