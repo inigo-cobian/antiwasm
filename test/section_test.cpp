a1d8850ef81946a28da97ec87755dd7e52537781
inigogc <inigogc@opendeusto.es>
2021-03-03 14:48:26 +0100

    test: added test Section_canBeCreated

--- test/section_test.cpp
+++ test/section_test.cpp
@@ -4,4 +4,16 @@
 
 BOOST_AUTO_TEST_SUITE( section_test )
 
+BOOST_AUTO_TEST_CASE(Section_canBeCreated)
+{
+    int sectionSize = 20;
+    int sectionIndex = 128;
+    antiwasm::SectionId sectionId = antiwasm::SectionId::Type;
+    unsigned char * sectionContentMock = malloc(sizeof(unsigned char) * 20);
+
+    Section section(sectionIndex, sectionId, sectionSize, sectionContentMock);
+
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
