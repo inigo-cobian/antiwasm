b70210672f8a08cce9c09d71f9ec7aa065fcfdd9
inigogc <inigogc@opendeusto.es>
2021-03-03 14:54:23 +0100

    test: added test getSize_returnsCorrectSize

--- test/section_test.cpp
+++ test/section_test.cpp
@@ -28,5 +28,17 @@ BOOST_AUTO_TEST_CASE(getSectionId_returnsCorrectSectionId)
     BOOST_CHECK_EQUAL(sectionId, result);
 }
 
+BOOST_AUTO_TEST_CASE(getSize_returnsCorrectSize)
+{
+    int sectionSize = 20;
+    int sectionIndex = 128;
+    antiwasm::SectionId sectionId = antiwasm::SectionId::Type;
+    unsigned char * sectionContentMock = malloc(sizeof(unsigned char) * 20);
+    Section section(sectionIndex, sectionId, sectionSize, sectionContentMock);
+
+    auto result = section.getSize();
+
+    BOOST_CHECK_EQUAL(sectionSize, result);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
