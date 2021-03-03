78d672eb2db8e744d6899584432e093ec1fc77b3
inigogc <inigogc@opendeusto.es>
2021-03-03 19:49:04 +0100

    test: added tests for getters at section_test

--- test/section_test.cpp
+++ test/section_test.cpp
@@ -41,4 +41,48 @@ BOOST_AUTO_TEST_CASE(getSize_returnsCorrectSize)
     BOOST_CHECK_EQUAL(sectionSize, result);
 }
 
+BOOST_AUTO_TEST_CASE(getContent_returnsAPointerWithTheSameInfo)
+{
+    int sectionSize = 4;
+    int sectionIndex = 128;
+    antiwasm::SectionId sectionId = antiwasm::SectionId::Type;
+    unsigned char * sectionContent = static_cast<unsigned char *>(malloc(sizeof(unsigned char) * sectionSize));
+    sectionContent[0] = 0xCA; sectionContent[1] = 0xFE; sectionContent[0] = 0x19; sectionContent[1] = 0x97;
+    Section section(sectionId, sectionSize, sectionContent, sectionIndex);
+
+    auto* result = section.getContent();
+
+    for(int i = 0; i < sectionSize; i++) {
+        BOOST_CHECK_EQUAL(sectionContent[i], result[i]);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(getContent_returnsAPointerToTheSameDirection)
+{
+    int sectionSize = 4;
+    int sectionIndex = 128;
+    antiwasm::SectionId sectionId = antiwasm::SectionId::Type;
+    unsigned char * sectionContent = static_cast<unsigned char *>(malloc(sizeof(unsigned char) * sectionSize));
+    sectionContent[0] = 0xCA; sectionContent[1] = 0xFE; sectionContent[0] = 0x19; sectionContent[1] = 0x97;
+    Section section(sectionId, sectionSize, sectionContent, sectionIndex);
+
+    auto* result = section.getContent();
+
+    BOOST_CHECK_EQUAL(sectionContent, result);
+}
+
+BOOST_AUTO_TEST_CASE(getInitialPos_returnsTheValue)
+{
+    int sectionSize = 4;
+    int sectionIndex = 128;
+    antiwasm::SectionId sectionId = antiwasm::SectionId::Type;
+    unsigned char * sectionContent = static_cast<unsigned char *>(malloc(sizeof(unsigned char) * sectionSize));
+    Section section(sectionId, sectionSize, sectionContent, sectionIndex);
+
+    auto result = section.getInitialPos();
+
+    BOOST_CHECK_EQUAL(sectionIndex, result);
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
