1fab68209fa769461e0083a0f1d7d725515365c9
inigogc <inigogc@opendeusto.es>
2021-03-03 17:05:36 +0100

    feat: generated getters and constructor for Section

--- src/section.cpp
+++ src/section.cpp
@@ -1,11 +1,24 @@
 #include "../includes/section.hpp"
 
-antiwasm::SectionId sectionId_;
-
-antiwasm::SectionId getSectionId() {
-    return sectionId_;
+Section::Section(antiwasm::SectionId sectionId, int size, unsigned char *content, int initialPos) {
+    sectionId_ = sectionId;
+    size_ = size;
+    content_ = content;
+    initialPos_ = initialPos;
 }
 
 antiwasm::SectionId Section::getSectionId() {
-    return antiwasm::Type;
+    return sectionId_;
+}
+
+int Section::getSize() {
+    return size_;
+}
+
+unsigned char *Section::getContent() {
+    return content_;
+}
+
+int Section::getInitialPos() {
+    return initialPos_;
 }
