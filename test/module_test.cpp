40518c42b1c80d2f2c5bea3a9d0973ba21c161ae
inigogc <inigogc@opendeusto.es>
2021-03-03 14:08:07 +0100

    test: added a new test Module_returnsItsSize

--- test/module_test.cpp
+++ test/module_test.cpp
@@ -4,6 +4,16 @@
 
 BOOST_AUTO_TEST_SUITE( module_test )
 
+BOOST_AUTO_TEST_CASE(Module_returnsItsSize)
+{
+    int sizeOfModule = 100;
+    Module module(sizeOfModule);
+
+    auto returnValue = module.getSize();
+
+    BOOST_CHECK_EQUAL(sizeOfModule, returnValue);
+}
+
 
 
 BOOST_AUTO_TEST_SUITE_END()
