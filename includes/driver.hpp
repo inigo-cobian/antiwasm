08a99f2609e1b1d0453bb448839aed01b5788ac1
inigogc <inigogc@opendeusto.es>
2021-03-04 21:53:23 +0100

    style: added comments to driver.hpp

--- includes/driver.hpp
+++ includes/driver.hpp
@@ -29,28 +29,70 @@ protected:
     static bool isParsing_;
 
 public:
+    /**
+     * Constructor
+     */
     Driver() {}
 
+    /**
+     * Destructor
+     */
     ~Driver() {}
 
-    Driver(Driver &driver) = delete;
-
+    /**
+     * Returns the single existing instance. If it doesn't exist, it is created.
+     * @return the instance
+     */
     static std::shared_ptr<Driver> GetInstance();
 
+    /**
+     * It returns the requested number of bytes
+     * @param nBytesToBeRead
+     * @return The bytes from the current position of the pointer until nBytesToBeRead.
+     */
     unsigned char *GetNextBytes(size_t nBytesToBeRead);
 
+    /**
+     * Returns the next 2 bytes. Alias of GetNextBytes when it is used for section headers.
+     * @return The next 2 bytes
+     */
     unsigned char *GetNextSectionHeader();
 
+    /**
+     * TODO
+     * @return
+     */
     unsigned char *GetUTF8String();
 
+    /**
+     * Opens the file at the given position.
+     * @param fileName
+     * @return true if the file exists, false if it doesn't.
+     */
     bool OpenFile(const char *fileName);
 
+    /**
+     * It closes the file that is currently being parsed.
+     */
     void CloseFile();
 
+    /**
+     * Checks if a file is being parsed.
+     * @return true if it is parsing a file, false if it is not.
+     */
     bool IsCurrentlyParsing();
 
-    static bool HasReachedFileSize(size_t nextBytesSize);
+    /**
+     * Checks if the next request would reach the end of the file.
+     * @param nextBytesSize
+     * @return true if it has reached the end of the file, false if it has not.
+     */
+    bool HasReachedFileSize(size_t nextBytesSize);
 
+    /**
+     * Gets the size of the file.
+     * @return the size of the file. TODO 0 if there is no file.
+     */
     int GetFileSize();
 };
 
