67e1ace75e002b89f471e44d12f9a8b545419e49
inigogc <inigogc@opendeusto.es>
2021-03-04 22:16:27 +0100

    style: added comments to module_parser.hpp

--- includes/module_parser.hpp
+++ includes/module_parser.hpp
@@ -10,30 +10,109 @@
 
 
 namespace antiwasm {
+    /**
+     * Generates a section with the given Id and returns it as an object.
+     * @param sectionId
+     * @param sectionSize
+     * @param sectionContent
+     * @return The parsed section.
+     */
     Section parseNextSection(unsigned char sectionId, int sectionSize, unsigned char *sectionContent);
 
+    /**
+     * Parses a Custom section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Custom section.
+     */
     int parseCustomSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses a Type section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Type section.
+     */
     int parseTypeSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses an Import section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Import section.
+     */
     int parseImportSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses a Function section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Function section.
+     */
     int parseFunctionSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses a Table section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Table section.
+     */
     int parseTableSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses a Memory section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Memory section.
+     */
     int parseMemorySection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses a Global section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Global section.
+     */
     int parseGlobalSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses an Export section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Export section.
+     */
     int parseExportSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses a Start section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Start section.
+     */
     int parseStartSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses an Element section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Element section.
+     */
     int parseElementSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses a Code section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Code section.
+     */
     int parseCodeSection(int sizeOfSection, unsigned char *sectionContent);
 
+    /**
+     * Parses a Data section and generates its instance.
+     * @param sizeOfSection
+     * @param sectionContent
+     * @return The parsed Data section.
+     */
     int parseDataSection(int sizeOfSection, unsigned char *sectionContent);
 }
 
