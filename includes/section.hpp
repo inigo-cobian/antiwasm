13648b03abfa8c3c9f4cc52ba381a7384ebcc291
inigogc <inigogc@opendeusto.es>
2021-03-04 22:27:29 +0100

    style: added comments to section.hpp

--- includes/section.hpp
+++ includes/section.hpp
@@ -10,17 +10,42 @@ private:
     unsigned char *content_;
     int initialPos_;
 public:
+    /**
+     * Constructor
+     * @param sectionId
+     * @param size
+     * @param content
+     * @param initialPos
+     */
     Section(antiwasm::SectionId sectionId, int size, unsigned char *content, int initialPos);
 
+    /**
+     * Destructor
+     */
     ~Section() {}
 
-    //Getters
+    /**
+     * Gets the Id of the section.
+     * @return The section Id.
+     */
     antiwasm::SectionId getSectionId();
 
+    /**
+     * Gets the size of the section in bytes.
+     * @return The size of the section.
+     */
     int getSize();
 
+    /**
+     * Gets the content of the section as bytes.
+     * @return The content of the section as bytes
+     */
     unsigned char *getContent();
 
+    /**
+     * Gets the initial position of the section inside the module.
+     * @return The initial position inside the module.
+     */
     int getInitialPos();
 };
 
