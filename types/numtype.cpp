383adff291d41bfd81766316d38bc0ed5b2eda15
inigogc <inigogc@opendeusto.es>
2021-03-17 21:32:00 +0100

    feat: created numtype

---
+++ types/numtype.cpp
@@ -0,0 +1,18 @@
+#include "numtype.hpp"
+
+namespace antiwasm {
+    Numtype parseNumType(const unsigned char numtypeContent) {
+        if(numtypeContent = i32) {
+            return i32;
+        } else if(numtypeContent = i64) {
+            return i64;
+        } else if(numtypeContent = f32) {
+            return f32;
+        } else if(numtypeContent = f64) {
+            return f64;
+        } else {
+            //TODO error message
+            return i32;
+        }
+    }
+}
---
+++ types/numtype.hpp
@@ -0,0 +1,22 @@
+#ifndef ANTIWASM_NUMTYPE_HPP
+#define ANTIWASM_NUMTYPE_HPP
+
+#include <cstdint>
+
+enum Numtype : int32_t {
+    i32 = 0x7F,
+    i64 = 0x7E,
+    f32 = 0x7D,
+    f64 = 0x7C
+};
+
+namespace antiwasm {
+    /**
+     * Gets a byte and returns the equivalent NumType
+     * @param numtypeContent
+     * @return
+     */
+    Numtype parseNumType(const unsigned char numtypeContent);
+}
+
+#endif //ANTIWASM_NUMTYPE_HPP
