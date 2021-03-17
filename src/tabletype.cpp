74f9dddfc9783ae9e7fbb78269531977c64f92c4
inigogc <inigogc@opendeusto.es>
2021-03-17 15:35:25 +0100

    style: initialized tabletype

--- src/tabletype.cpp
+++ src/tabletype.cpp
@@ -2,7 +2,7 @@
 
 namespace antiwasm {
     Tabletype parseTableType(const unsigned char *tableTypeContent) {
-        Tabletype tabletype;
+        Tabletype tabletype{};
         tabletype.reftype = parseReftype(tableTypeContent[0]);
         tabletype.limit = parseLimits(tableTypeContent);
         return tabletype;
