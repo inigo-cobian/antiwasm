0020ecd12695d6557e16d60268a036030be26f33
inigogc <inigogc@opendeusto.es>
2021-03-19 16:22:45 +0100

    test: finished numtype_test tests

--- test/numtype_test.cpp
+++ test/numtype_test.cpp
@@ -13,5 +13,37 @@ BOOST_AUTO_TEST_SUITE(numtype_test)
         BOOST_CHECK_EQUAL(Numtype::i32, returnNumtype);
     }
 
+    BOOST_AUTO_TEST_CASE(parseNumType_casei64) {
+        unsigned char numtypeContent = 0x7E;
+
+        auto returnNumtype = antiwasm::parseNumType(numtypeContent);
+
+        BOOST_CHECK_EQUAL(Numtype::i64, returnNumtype);
+    }
+
+    BOOST_AUTO_TEST_CASE(parseNumType_casef32) {
+        unsigned char numtypeContent = 0x7D;
+
+        auto returnNumtype = antiwasm::parseNumType(numtypeContent);
+
+        BOOST_CHECK_EQUAL(Numtype::f32, returnNumtype);
+    }
+
+    BOOST_AUTO_TEST_CASE(parseNumType_casef64) {
+        unsigned char numtypeContent = 0x7C;
+
+        auto returnNumtype = antiwasm::parseNumType(numtypeContent);
+
+        BOOST_CHECK_EQUAL(Numtype::f64, returnNumtype);
+    }
+
+    BOOST_AUTO_TEST_CASE(parseNumType_caseInvalid) {
+        unsigned char numtypeContent = 0xCA;
+
+        auto returnNumtype = antiwasm::parseNumType(numtypeContent);
+
+        BOOST_CHECK_EQUAL(Numtype::invalid, returnNumtype);
+    }
+
 
 BOOST_AUTO_TEST_SUITE_END() //numtype_test
