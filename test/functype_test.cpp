ddd1ca0859600a3685a23d011a6758c1983fe2eb
inigogc <inigogc@opendeusto.es>
2021-03-21 00:42:42 +0100

    test: created tests for functype

--- test/functype_test.cpp
+++ test/functype_test.cpp
@@ -5,12 +5,26 @@
 
 BOOST_AUTO_TEST_SUITE(functype_test)
 
-    BOOST_AUTO_TEST_CASE(parseFunctype) {
-        unsigned char *functypeContent = nullptr; //TODO
+    BOOST_AUTO_TEST_CASE(parseFunctype_caseCorrectlyFormed) {
+        unsigned char *functypeContent = static_cast<unsigned char *>(malloc(sizeof(unsigned char) * 6));
+        functypeContent[0] = FUNCTYPE_HEADER;
+        functypeContent[1] = 2, functypeContent[2] = Numtype::i32, functypeContent[3] = Reftype::funref;
+        functypeContent[4] = 1, functypeContent[5] = Numtype::f64;
 
         auto returnFunctype = antiwasm::parseFunctype(functypeContent);
 
-        //BOOST_CHECK_EQUAL(Functype, returnFunctype); TODO
+        BOOST_CHECK_EQUAL(false, returnFunctype.error);
+    }
+
+    BOOST_AUTO_TEST_CASE(parseFunctype_caseIncorrectHeader) {
+        unsigned char *functypeContent = static_cast<unsigned char *>(malloc(sizeof(unsigned char) * 6));
+        functypeContent[0] = 0x99; //Invalid header
+        functypeContent[1] = 2, functypeContent[2] = Numtype::i32, functypeContent[3] = Reftype::funref; // Should be ignored
+        functypeContent[4] = 1, functypeContent[5] = Numtype::f64; // Should be ignored
+
+        auto returnFunctype = antiwasm::parseFunctype(functypeContent);
+
+        BOOST_CHECK_EQUAL(true, returnFunctype.error);
     }
 
 BOOST_AUTO_TEST_SUITE_END() //functype_test
