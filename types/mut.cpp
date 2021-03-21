0a3a2e90ebc8511285e2ed7aebb5311843d308bf
inigogc <inigogc@opendeusto.es>
2021-03-21 14:43:43 +0100

    feat: created mut

---
+++ types/mut.cpp
@@ -0,0 +1,7 @@
+#include "globaltype.hpp"
+
+namespace antiwasm {
+    Mut parseMut(const unsigned char mutContent) {
+        return Mut::const_; //TODO
+    }
+}
---
+++ types/mut.hpp
@@ -0,0 +1,14 @@
+#ifndef ANTIWASM_MUT_HPP
+#define ANTIWASM_MUT_HPP
+
+enum Mut : int32_t {
+    const_ = 0x00,
+    var_ = 0x01,
+    invalid_mut = 0xAA;
+};
+
+namespace antiwasm {
+    Mut parseMut(const unsigned char mutContent);
+}
+
+#endif //ANTIWASM_MUT_HPP
