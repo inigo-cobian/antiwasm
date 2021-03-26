8a6e33ca6026d8fcd32a6985a3369533d8828de0
inigogc <inigogc@opendeusto.es>
2021-03-26 20:43:08 +0100

    style: removed commented code

--- utils/leb128.cpp
+++ utils/leb128.cpp
@@ -23,33 +23,6 @@ uint32_t antiwasm::transformLeb128ToUnsignedInt32(uint8_t *leb128) {
     }
 
     return result;
-
-    /*
-    int pos = 0;
-    uint32_t decodedValue = 0;
-    int shift = 0;
-    do {
-        uint32_t currentShift = (leb128[pos] & 0b0111'1111);
-        std::cout << "AAA: " << std::hex << currentShift << std::endl;
-        std::cout << "pre: " << std::hex << decodedValue << std::endl;
-        decodedValue <<= 7;
-        std::cout << "pos: " << std::hex << decodedValue << std::endl;
-        decodedValue |= currentShift;
-        std::cout << "BBB: " << std::hex << decodedValue << std::endl;
-        std::cout << "CCC: " << std::hex << (decodedValue >> 7) << std::endl;
-
-
-        if ((leb128[pos] & 1000'0000) == 0) {
-            std::cout << "XXX: " << std::hex << (decodedValue) << std::endl;
-            std::cout << "YYY: " << std::dec << decodedValue << std::endl;
-            return decodedValue;
-        }
-
-        pos++;
-    } while (true);
-
-    return decodedValue;
-    */
 }
 
 uint64_t antiwasm::transformLeb128ToUnsignedInt64(uint8_t *leb128) {
