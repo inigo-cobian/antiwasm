60ec3c56c5c7b4a1b0ac1fd398465f86a103be0d
inigogc <inigogc@opendeusto.es>
2021-04-09 17:05:11 +0200

    feat: created Error struct

---
+++ utils/error.hpp
@@ -0,0 +1,24 @@
+#ifndef ANTIWASM_ERROR_HPP
+#define ANTIWASM_ERROR_HPP
+
+enum error_types {
+    unknown,
+    incorrectHeader,
+    unrecognizedVersion,
+    wrongSectionId
+};
+
+enum error_severity {
+    fatal,
+    warning
+};
+
+struct Error {
+    Section *section;
+    unsigned int elementIndex;
+    error_types errorType;
+    error_severity severity;
+};
+
+
+#endif //ANTIWASM_ERROR_HPP
