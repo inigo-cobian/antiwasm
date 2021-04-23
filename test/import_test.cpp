de6b78e6df2ba2224273022a421b82b85e43a156
inigogc <inigogc@opendeusto.es>
2021-04-23 14:34:26 +0200

    test: created structure of importsec and import tests

---
+++ test/import_test.cpp
@@ -0,0 +1,26 @@
+#define BOOST_TEST_DYN_LINK
+
+#include "import.hpp"
+#include <boost/test/unit_test.hpp>
+
+using namespace antiwasm;
+
+BOOST_AUTO_TEST_SUITE(import_test)
+
+BOOST_AUTO_TEST_CASE(parseImport_correct) {
+  // TODO
+}
+
+BOOST_AUTO_TEST_CASE(parseImport_moduleError) {
+  // TODO
+}
+
+BOOST_AUTO_TEST_CASE(parseImport_nameError) {
+  // TODO
+}
+
+BOOST_AUTO_TEST_CASE(parseImport_importDescError) {
+  // TODO
+}
+
+BOOST_AUTO_TEST_SUITE_END() // import_test
