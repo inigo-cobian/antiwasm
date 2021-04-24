f158a4fa8868321d0f3e18f12037377776683dda
inigogc <inigogc@opendeusto.es>
2021-04-24 20:04:12 +0200

    test: coded test parseImport_correct

--- test/import_test.cpp
+++ test/import_test.cpp
@@ -8,6 +8,20 @@ using namespace antiwasm;
 BOOST_AUTO_TEST_SUITE(import_test)
 
 BOOST_AUTO_TEST_CASE(parseImport_correct) {
+  auto sizeOfMod = 4;
+  auto sizeOfName = 3;
+  auto typeidx = 0x01;
+  auto *importContent = new uint8_t[11];
+  importContent[0] = sizeOfMod;
+  importContent[1] = 't', importContent[2] = 'e', importContent[3] = 's', importContent[4] = 't';
+  importContent[5] = sizeOfName;
+  importContent[6] = 'F', importContent[7] = 'o', importContent[8] = 'o';
+  importContent[9] = ImportDescType::ImportFunc;
+  importContent[10] = typeidx;
+
+  auto import = parseImport(importContent);
+
+  BOOST_CHECK_EQUAL(false, import.hasError());
   // TODO
 }
 
