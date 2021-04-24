51c94fcaf09c1ed6c346c4bae8b56f3f70170e6c
inigogc <inigogc@opendeusto.es>
2021-04-24 19:45:07 +0200

    style: moved ImportDesc to Import to better represent the grammar

--- types/import.cpp
+++ types/import.cpp
@@ -2,7 +2,20 @@
 
 namespace antiwasm {
 Import parseImport(const uint8_t *importContent) {
-  // TODO
+  // TODO mod name importdesc
+  switch (importContent[0]) {
+  case ImportFunc:
+    break;
+  case ImportTable:
+    break;
+  case ImportMemtype:
+    break;
+  case ImportGlobaltype:
+    break;
+  default:
+    // TODO error
+    break;
+  }
   return Import{};
 }
 } // namespace antiwasm
--- types/import.hpp
+++ types/import.hpp
@@ -2,14 +2,27 @@
 #define ANTIWASM_IMPORT_HPP
 
 #include "contentBlock.hpp"
-#include "importDesc.hpp"
+#include "globaltype.hpp"
+#include "memtype.hpp"
 #include "nameParser.hpp"
+#include "tabletype.hpp"
+#include <cstdint>
 
 namespace antiwasm {
+
+enum ImportDescType : uint8_t { ImportFunc = 0x00, ImportTable = 0x01, ImportMemtype = 0x02, ImportGlobaltype = 0x03 };
+
+union ImportDesc {
+  uint32_t typeIdx;
+  Tabletype tabletype;
+  Memtype memtype;
+  Globaltype globaltype;
+};
 
 struct Import {
   UTF8Name moduleName;
   UTF8Name componentName;
+  ImportDescType importDescType;
   ImportDesc importDesc;
 };
 
