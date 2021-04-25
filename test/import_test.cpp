10419eeaecd276380587a95a0fed69ff0e98a0df
inigogc <inigogc@opendeusto.es>
2021-04-25 13:45:24 +0200

    test: added new tests for name and module at import_test

--- test/import_test.cpp
+++ test/import_test.cpp
@@ -22,18 +22,53 @@ BOOST_AUTO_TEST_CASE(parseImport_correct) {
   auto import = parseImport(importContent);
 
   BOOST_CHECK_EQUAL(false, import.hasError());
-  // TODO
 }
 
 BOOST_AUTO_TEST_CASE(parseImport_moduleError) {
-  // TODO
+  auto sizeOfMod = 4;
+  auto sizeOfName = 3;
+  auto typeidx = 0x01;
+  auto *importContent = new uint8_t[11];
+  importContent[0] = sizeOfMod;
+  importContent[1] = 't', importContent[2] = 'e', importContent[3] = 's';
+  importContent[4] = 0b10001000; // Invalid byte
+  importContent[5] = sizeOfName;
+  importContent[6] = 'F', importContent[7] = 'o', importContent[8] = 'o';
+  importContent[9] = ImportDescType::ImportFunc;
+  importContent[10] = typeidx;
+
+  auto import = parseImport(importContent);
+
+  BOOST_CHECK_EQUAL(ErrorTypes::unrecognizedModAtImport, import.getError()->errorType);
 }
 
 BOOST_AUTO_TEST_CASE(parseImport_nameError) {
+  auto sizeOfMod = 4;
+  auto sizeOfName = 3;
+  auto typeidx = 0x01;
+  auto *importContent = new uint8_t[11];
+  importContent[0] = sizeOfMod;
+  importContent[1] = 't', importContent[2] = 'e', importContent[3] = 's', importContent[4] = 't';
+  importContent[5] = sizeOfName;
+  importContent[6] = 'F', importContent[7] = 'o';
+  importContent[8] = 0b10001000; // Invalid byte
+  importContent[9] = ImportDescType::ImportFunc;
+  importContent[10] = typeidx;
+
+  auto import = parseImport(importContent);
+
+  BOOST_CHECK_EQUAL(ErrorTypes::unrecognizedModAtImport, import.getError()->errorType);
+}
+
+BOOST_AUTO_TEST_CASE(parseImport_importDescTabletypeError) {
   // TODO
 }
 
-BOOST_AUTO_TEST_CASE(parseImport_importDescError) {
+BOOST_AUTO_TEST_CASE(parseImport_importDescMemtypeError) {
+  // TODO
+}
+
+BOOST_AUTO_TEST_CASE(parseImport_importDescGlobaltypeError) {
   // TODO
 }
 
