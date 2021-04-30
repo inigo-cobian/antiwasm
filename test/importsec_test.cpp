d95f80cc862a8a928f9231271ce03fdf81f15f49
inigogc <inigogc@opendeusto.es>
2021-04-30 18:28:10 +0200

    test: defined tests for importsec_test

--- test/importsec_test.cpp
+++ test/importsec_test.cpp
@@ -5,10 +5,64 @@
 
 using namespace antiwasm;
 
-BOOST_AUTO_TEST_SUITE(import_test)
+uint8_t *validImportGenerator() {
+  auto sizeOfMod = 4;
+  auto sizeOfName = 3;
+  auto typeidx = 0x01;
 
-BOOST_AUTO_TEST_CASE(todo) {
-  // TODO
+  auto *importContent = new uint8_t[11];
+  importContent[0] = sizeOfMod;
+  importContent[1] = 't', importContent[2] = 'e', importContent[3] = 's', importContent[4] = 't';
+  importContent[5] = sizeOfName;
+  importContent[6] = 'F', importContent[7] = 'o', importContent[8] = 'o';
+  importContent[9] = ImportDescType::ImportFunc;
+  importContent[10] = typeidx;
+  return importContent;
 }
+
+BOOST_AUTO_TEST_SUITE(importsec_test)
+
+BOOST_AUTO_TEST_CASE(importsec_constructor) {
+  int size = 8;
+  auto *content = new uint8_t[size];
+  int initialPos = 0;
+
+  ImportSection importSec(size, content, initialPos);
+
+  BOOST_CHECK_EQUAL(SectionId::ImportId, importSec.getSectionId());
+}
+
+BOOST_AUTO_TEST_CASE(addImport_addsTheImportToTheVector) {
+  int size = 8;
+  auto *content = new uint8_t[size];
+  int initialPos = 0;
+  ImportSection importSec(size, content, initialPos);
+  Import importMock = parseImport(validImportGenerator());
+
+  importSec.addImport(importMock);
+
+  BOOST_CHECK_EQUAL(1, importSec.getImportVector().size());
+}
+
+BOOST_AUTO_TEST_CASE(displaySectionHeaderInfo_doesNotFail) {
+  int size = 8;
+  auto *content = new uint8_t[size];
+  int initialPos = 0;
+  ImportSection importSec(size, content, initialPos);
+
+  importSec.displaySectionHeaderInfo();
+
+}
+
+BOOST_AUTO_TEST_CASE(displaySectionContentInfo_doesNotFail) {
+  int size = 8;
+  auto *content = new uint8_t[size];
+  int initialPos = 0;
+  ImportSection importSec(size, content, initialPos);
+
+  importSec.displaySectionHeaderInfo();
+
+}
+
 
 BOOST_AUTO_TEST_SUITE_END() // import_test
