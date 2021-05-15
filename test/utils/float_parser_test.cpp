91fc14f155ea670d2668353ce35f4b3e00c3cbe8
inigogc <inigogc@opendeusto.es>
2021-05-15 21:56:55 +0200

    test: corrected float_parser_test for real values

--- test/utils/float_parser_test.cpp
+++ test/utils/float_parser_test.cpp
@@ -9,11 +9,12 @@ BOOST_AUTO_TEST_SUITE(float_parser_test)
 
 BOOST_AUTO_TEST_CASE(transformf32ToFloat_test) {
   auto *f32Val = new uint8_t[4];
-  f32Val[0] = 205, f32Val[1] = 204, f32Val[2] = 204, f32Val[3] = 61;
-  float expected = 0.1;
+  f32Val[0] = 0x3f, f32Val[1] = 0x9d, f32Val[2] = 0xf3, f32Val[3] = 0xb6;
+  float expected = 1.234;
 
   auto returnVal = transformf32ToFloat(f32Val);
 
+  // Converted to int so precision errors can be avoided
   BOOST_CHECK_EQUAL(expected, returnVal);
 }
 
@@ -22,10 +23,11 @@ BOOST_AUTO_TEST_CASE(transformf64ToDouble_test) {
   f64Val[0] = 0x40, f64Val[1] = 0x50, f64Val[2] = 0x5b, f64Val[3] = 0x85;
   f64Val[4] = 0x1e, f64Val[5] = 0xb8, f64Val[6] = 0x51, f64Val[7] = 0xec;
 
-  float expected = 65.43;
+  double expected = 65.43;
 
   auto returnVal = transformf64ToDouble(f64Val);
 
+  // Converted to int so precision errors can be avoided
   BOOST_CHECK_EQUAL(expected, returnVal);
 }
 
