d43dffc74be226e293d8d05ed8193703814c5ba9
inigogc <inigogc@opendeusto.es>
2021-05-16 14:12:27 +0200

    fix: fixed missing instrCode at loads

--- instructions/memory/load/i32_load.cpp
+++ instructions/memory/load/i32_load.cpp
@@ -2,6 +2,7 @@
 
 namespace antiwasm {
 I32Load::I32Load(const uint8_t *memarg_) : memArg(memarg_){
+  instructionCode = i32_load;
   nBytes = memArg.getNBytes() + 1;
 }
 } // namespace antiwasm
--- instructions/memory/load/i64_load.cpp
+++ instructions/memory/load/i64_load.cpp
@@ -2,6 +2,7 @@
 
 namespace antiwasm {
 I64Load::I64Load(const uint8_t *memarg_) : memArg(memarg_){
+  instructionCode = i64_load;
   nBytes = memArg.getNBytes() + 1;
 }
 } // namespace antiwasm
