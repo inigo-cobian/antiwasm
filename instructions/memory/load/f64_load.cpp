09f14308cfb57d2af2392218e8b57635729e7f44
inigogc <inigogc@opendeusto.es>
2021-05-16 14:23:36 +0200

    fix: fixed wrong instrCode

--- instructions/memory/load/f64_load.cpp
+++ instructions/memory/load/f64_load.cpp
@@ -2,7 +2,7 @@
 
 namespace antiwasm {
 F64Load::F64Load(const uint8_t *memarg_) : memArg(memarg_){
-  instructionCode = f32_load;
+  instructionCode = f64_load;
   nBytes = memArg.getNBytes() + 1;
 }
 } // namespace antiwasm
