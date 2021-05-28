a2f2100a43aa7cc96691d0755bacf361ee99c1a6
inigogc <inigogc@opendeusto.es>
2021-05-28 21:32:46 +0200

    fix: corrected if conditions at errorInstr

--- instructions/errorInstr.cpp
+++ instructions/errorInstr.cpp
@@ -4,13 +4,14 @@ namespace antiwasm {
 ErrorInstr::ErrorInstr(const uint8_t *instrContent) {
   instructionCode = error_instr;
 
-  if (invalidInstrByte == double_byte_instr) {
+  if (instrContent[0] == double_byte_instr) {
+
     auto error = generateError(fatal, unrecognizedDoubleByteInstruction, 1);
     addError(error);
     invalidInstrByte = instrContent[1];
     nBytes = 2;
   }
-  else if(invalidInstrByte == instrContent[0]) {
+  else if(instrContent[0] == Else ) {
     auto error = generateError(fatal, unlinkedElseAtInstruction, 0);
     addError(error);
     invalidInstrByte = instrContent[0];
