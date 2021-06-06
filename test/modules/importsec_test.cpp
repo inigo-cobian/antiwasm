dce5020523bc27a37b7c82c2137e011896fc212e
inigogc <inigogc@opendeusto.es>
2021-06-06 15:33:57 +0200

    test: modified import tests to increase coverage

--- test/modules/importsec_test.cpp
+++ test/modules/importsec_test.cpp
@@ -21,7 +21,7 @@ BOOST_AUTO_TEST_CASE(addImport_addsTheImportToTheVector) {
   auto *content = new uint8_t[size];
   int initialPos = 0;
   ImportSection importSec(size, content, initialPos);
-  Import importMock = parseImport(validImportGenerator());
+  Import importMock = parseImport(validImportFuncGenerator());
 
   importSec.addImport(importMock);
 
@@ -35,15 +35,67 @@ BOOST_AUTO_TEST_CASE(displaySectionHeaderInfo_doesNotFail) {
   ImportSection importSec(size, content, initialPos);
 
   importSec.displaySectionHeaderInfo();
+
 }
 
-BOOST_AUTO_TEST_CASE(displaySectionContentInfo_doesNotFail) {
+BOOST_AUTO_TEST_CASE(displaySectionContentInfo_caseImportFunc) {
   int size = 8;
   auto *content = new uint8_t[size];
   int initialPos = 0;
   ImportSection importSec(size, content, initialPos);
+  Import importMock = parseImport(validImportFuncGenerator());
+  importSec.addImport(importMock);
 
-  importSec.displaySectionHeaderInfo();
+  importSec.displaySectionContentInfo();
+
+}
+
+BOOST_AUTO_TEST_CASE(displaySectionContentInfo_caseImportTable) {
+  int size = 8;
+  auto *content = new uint8_t[size];
+  int initialPos = 0;
+  Import importMock = parseImport(validImportTableGenerator());
+  ImportSection importSec(size, content, initialPos);
+  importSec.addImport(importMock);
+
+  importSec.displaySectionContentInfo();
+
+}
+
+BOOST_AUTO_TEST_CASE(displaySectionContentInfo_caseImportMemtype) {
+  int size = 8;
+  auto *content = new uint8_t[size];
+  int initialPos = 0;
+  Import importMock = parseImport(validImportMemtypeGenerator());
+  ImportSection importSec(size, content, initialPos);
+  importSec.addImport(importMock);
+
+  importSec.displaySectionContentInfo();
+
+}
+
+BOOST_AUTO_TEST_CASE(displaySectionContentInfo_caseImportGlobaltype) {
+  int size = 8;
+  auto *content = new uint8_t[size];
+  int initialPos = 0;
+  Import importMock = parseImport(validImportGlobalGenerator());
+  ImportSection importSec(size, content, initialPos);
+  importSec.addImport(importMock);
+
+  importSec.displaySectionContentInfo();
+
+}
+
+BOOST_AUTO_TEST_CASE(displaySectionContentInfo_caseImportInvalid) {
+  int size = 8;
+  auto *content = new uint8_t[size];
+  int initialPos = 0;
+  Import importMock = parseImport(invalidImportGenerator());
+  ImportSection importSec(size, content, initialPos);
+  importSec.addImport(importMock);
+
+  importSec.displaySectionContentInfo();
+
 }
 
 BOOST_AUTO_TEST_SUITE_END() // import_test
--- test/modules/importsec_test.hpp
+++ test/modules/importsec_test.hpp
@@ -4,7 +4,7 @@
 #include "importsec.hpp"
 #include <boost/test/unit_test.hpp>
 
-uint8_t *validImportGenerator() {
+uint8_t *validImportFuncGenerator() {
   auto sizeOfMod = 4;
   auto sizeOfName = 3;
   auto typeidx = 0x01;
@@ -16,6 +16,68 @@ uint8_t *validImportGenerator() {
   importContent[6] = 'F', importContent[7] = 'o', importContent[8] = 'o';
   importContent[9] = antiwasm::ImportDescType::ImportFunc;
   importContent[10] = typeidx;
+  return importContent;
+}
+
+uint8_t *validImportTableGenerator() {
+  auto sizeOfMod = 4;
+  auto sizeOfName = 3;
+  auto *importContent = new uint8_t[11];
+  importContent[0] = sizeOfMod;
+  importContent[1] = 't', importContent[2] = 'e', importContent[3] = 's', importContent[4] = 't';
+  importContent[5] = sizeOfName;
+  importContent[6] = 'F', importContent[7] = 'o', importContent[8] = 'o';
+  importContent[9] = antiwasm::ImportDescType::ImportTable;
+  importContent[10] = antiwasm::Reftype::externref;
+  importContent[11] = antiwasm::limit_types::limit_min;
+  importContent[12] = 0x02;
+
+  return importContent;
+}
+
+uint8_t *validImportMemtypeGenerator() {
+  auto sizeOfMod = 4;
+  auto sizeOfName = 3;
+  auto typeidx = 0x01;
+  auto *importContent = new uint8_t[12];
+  importContent[0] = sizeOfMod;
+  importContent[1] = 't', importContent[2] = 'e', importContent[3] = 's', importContent[4] = 't';
+  importContent[5] = sizeOfName;
+  importContent[6] = 'F', importContent[7] = 'o', importContent[8] = 'o';
+  importContent[9] = antiwasm::ImportDescType::ImportMemtype;
+  importContent[10] = antiwasm::limit_types::limit_min;
+  importContent[11] = 0x02;
+
+  return importContent;
+}
+
+uint8_t *validImportGlobalGenerator() {
+  auto sizeOfMod = 4;
+  auto sizeOfName = 3;
+  auto *importContent = new uint8_t[12];
+  importContent[0] = sizeOfMod;
+  importContent[1] = 't', importContent[2] = 'e', importContent[3] = 's', importContent[4] = 't';
+  importContent[5] = sizeOfName;
+  importContent[6] = 'F', importContent[7] = 'o', importContent[8] = 'o';
+  importContent[9] = antiwasm::ImportDescType::ImportGlobaltype;
+  importContent[10] = Mut::var_;
+  importContent[11] = antiwasm::Reftype::externref;
+
+  return importContent;
+}
+
+uint8_t *invalidImportGenerator() {
+  auto sizeOfMod = 4;
+  auto sizeOfName = 3;
+  auto *importContent = new uint8_t[12];
+  importContent[0] = sizeOfMod;
+  importContent[1] = 't', importContent[2] = 'e', importContent[3] = 's', importContent[4] = 't';
+  importContent[5] = sizeOfName;
+  importContent[6] = 'F', importContent[7] = 'o', importContent[8] = 'o';
+  importContent[9] = antiwasm::ImportDescType::invalidImportDescType;
+  importContent[10] = Mut::var_;
+  importContent[11] = antiwasm::Reftype::externref;
+
   return importContent;
 }
 
