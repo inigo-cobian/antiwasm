cb182d73886da55540d4043beee989347449586e
inigogc <inigogc@opendeusto.es>
2021-06-07 19:41:10 +0200

    feat: added datasec section

---
+++ modules/datasec.cpp
@@ -0,0 +1,31 @@
+#include "datasec.hpp"
+
+namespace antiwasm {
+
+DataSection::DataSection(int size, uint8_t *content, int initialPos)
+    : Section(SectionId::DataId, size, content, initialPos) {
+  BOOST_LOG_TRIVIAL(debug) << "[datasec] Creating a Data Section";
+}
+
+DataSection::~DataSection() = default;
+
+void DataSection::addData(const Data &data) { dataVector.push_back(data); }
+
+std::vector<Data> DataSection::getDataVector() { return dataVector; }
+
+void DataSection::displaySectionHeaderInfo() {
+  std::cout << "    Data | start=" << std::hex << initialPos_ << " size=" << std::hex << size_ << std::endl;
+}
+
+void DataSection::displaySectionContentInfo() {
+  std::cout << "    Data | start=" << std::hex << initialPos_ << " size=" << std::hex << size_ << std::endl;
+
+  size_t index = 0;
+  std::for_each(dataVector.begin(), dataVector.end(),
+                [](const Data &data){std::cout << data.getAsText() << std::endl;}
+  );
+
+  std::cout << std::endl;
+
+}
+}
---
+++ modules/datasec.hpp
@@ -0,0 +1,56 @@
+#ifndef ANTIWASM_DATASEC_HPP
+#define ANTIWASM_DATASEC_HPP
+
+#include "expression.hpp"
+#include "data.hpp"
+#include "section.hpp"
+#include <sstream>
+#include <vector>
+
+namespace antiwasm {
+
+class DataSection : public Section {
+private:
+  std::vector<Data> dataVector;
+
+public:
+  /**
+   * Contructor.
+   * SectionId = SectionId::MemoryId
+   * @param size
+   * @param content
+   * @param initialPos
+   */
+  DataSection(int size, uint8_t *content, int initialPos);
+
+  /**
+   * Destructor
+   */
+  ~DataSection();
+
+  /**
+   * Adds a new data to the vector
+   * @param data
+   */
+  void addData(const Data &data);
+
+  /**
+   * Gets the data vector
+  * @return dataVector
+   */
+  std::vector<Data> getDataVector();
+
+  /**
+   * Displays the header information for the memory section.
+   */
+  void displaySectionHeaderInfo();
+
+  /**
+   * Displays the information of the datasec
+   */
+  void displaySectionContentInfo(); // override
+};
+
+} // namespace antiwasm
+
+#endif // ANTIWASM_DATASEC_HPP
