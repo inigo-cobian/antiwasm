e0cb039f8615e9c9e20eae3c396a5b3464606fa9
inigogc <inigogc@opendeusto.es>
2021-06-07 21:19:49 +0200

    feat: added individual section displaying

--- utils/displayer.cpp
+++ utils/displayer.cpp
@@ -4,6 +4,7 @@ using namespace std;
 namespace antiwasm {
 
 LoggingLevel Displayer::loggingLevel_;
+SectionId Displayer::sectionToDisplay{ErrorId};
 
 void Displayer::Log(LoggingLevel minLoggingLevel, const string &logMessage) {
   if (Displayer::loggingLevel_ >= minLoggingLevel) {
@@ -14,5 +15,15 @@ void Displayer::Log(LoggingLevel minLoggingLevel, const string &logMessage) {
 void Displayer::SetLoggingLevel(LoggingLevel loggingLevel) { Displayer::loggingLevel_ = loggingLevel; }
 
 LoggingLevel Displayer::GetLoggingLevel() { return Displayer::loggingLevel_; }
+
+bool Displayer::setSectionToDisplay(int sectionToDisplay_) {
+  if(sectionToDisplay_ >= 0 && sectionToDisplay_ <= 12) {
+    Displayer::sectionToDisplay = static_cast<SectionId>(sectionToDisplay_ - '0');
+    return true;
+  }
+  return false;
+}
+
+SectionId Displayer::getSectionToDisplay() { return Displayer::sectionToDisplay; }
 
 } // namespace antiwasm
--- utils/displayer.hpp
+++ utils/displayer.hpp
@@ -2,6 +2,7 @@
 #define ANTIWASM_DISPLAYER_HPP
 
 #include "logging_levels.hpp"
+#include "section.hpp" // for SectionId
 #include <iostream>
 
 namespace antiwasm {
@@ -9,6 +10,7 @@ namespace antiwasm {
 class Displayer { // TODO general
 private:
   static LoggingLevel loggingLevel_;
+  static SectionId sectionToDisplay;
 
 public:
   static void Log(LoggingLevel minLoggingLevel, const std::string &logMessage);
@@ -16,6 +18,10 @@ public:
   static void SetLoggingLevel(LoggingLevel loggingLevel);
 
   static LoggingLevel GetLoggingLevel();
+
+  static bool setSectionToDisplay(int sectionToDisplay_);
+
+  static SectionId getSectionToDisplay();
 };
 
 } // namespace antiwasm
