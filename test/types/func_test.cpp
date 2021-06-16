ee89549cad5c6e3a72997aa59f5d14fa7e51b5e0
inigogc <inigogc@opendeusto.es>
2021-06-16 14:44:45 +0200

    test: defined tests for func

--- test/types/func_test.cpp
+++ test/types/func_test.cpp
@@ -7,11 +7,11 @@ using namespace antiwasm;
 BOOST_AUTO_TEST_SUITE(func_test)
 
 BOOST_AUTO_TEST_CASE(parseFunc_caseCorrect) {
-  auto *funcContent = new uint8_t[4];
+  auto *funcContent = new uint8_t[6];
   funcContent[0] = 0x02; // localsVec size
   funcContent[1] = Numtype::i32;
   funcContent[2] = Numtype::f64;
-  funcContent[3] = InstructionSet::End; // expr
+  funcContent[3] = i32_const, funcContent[4] = 0x05, funcContent[5] = End; // expr
 
   auto func = parseFunc(funcContent);
 
@@ -28,6 +28,20 @@ BOOST_AUTO_TEST_CASE(parseFunc_caseInvalidLocals) {
   auto func = parseFunc(funcContent);
 
   BOOST_CHECK_EQUAL(true, func.hasError());
+  BOOST_CHECK_EQUAL(invalidLocalAtFunc, func.getError()->errorType);
+}
+
+BOOST_AUTO_TEST_CASE(parseFunc_caseInvalidExpr) {
+  auto *funcContent = new uint8_t[4];
+  funcContent[0] = 0x02; // localsVec size
+  funcContent[1] = Numtype::i32;
+  funcContent[2] = Reftype::funref;
+  funcContent[3] = InstructionSet::Else; // Invalid instr at this pos
+
+  auto func = parseFunc(funcContent);
+
+  BOOST_CHECK_EQUAL(true, func.hasError());
+  BOOST_CHECK_EQUAL(invalidExprAtFunc, func.getError()->errorType);
 }
 
 BOOST_AUTO_TEST_SUITE_END() // func_test
