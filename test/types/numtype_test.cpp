52dad6e1c73b19023225fcf0352a645bb9b85923
inigogc <inigogc@opendeusto.es>
2021-06-20 23:38:24 +0200

    test: adapted tests for numtype after changing getAsText

--- test/types/numtype_test.cpp
+++ test/types/numtype_test.cpp
@@ -46,39 +46,39 @@ BOOST_AUTO_TEST_CASE(parseNumType_caseInvalid) {
   BOOST_CHECK_EQUAL(Numtype::invalid_numtype, returnNumtype);
 }
 
-BOOST_AUTO_TEST_CASE(displayNumType_casei32) {
+BOOST_AUTO_TEST_CASE(getNumtypeAsText_casei32) {
   uint8_t numtypeContent = i32;
   auto returnNumtype = antiwasm::parseNumType(numtypeContent);
 
-  displayNumtype(returnNumtype);
+  auto str = getNumtypeAsText(returnNumtype);
 }
 
-BOOST_AUTO_TEST_CASE(displayNumType_casei64) {
+BOOST_AUTO_TEST_CASE(getNumtypeAsText_casei64) {
   uint8_t numtypeContent = i64;
   auto returnNumtype = antiwasm::parseNumType(numtypeContent);
 
-  displayNumtype(returnNumtype);
+  auto str = getNumtypeAsText(returnNumtype);
 }
 
-BOOST_AUTO_TEST_CASE(displayNumType_casef32) {
+BOOST_AUTO_TEST_CASE(getNumtypeAsText_casef32) {
   uint8_t numtypeContent = f32;
   auto returnNumtype = antiwasm::parseNumType(numtypeContent);
 
-  displayNumtype(returnNumtype);
+  auto str = getNumtypeAsText(returnNumtype);
 }
 
-BOOST_AUTO_TEST_CASE(displayNumType_casef64) {
+BOOST_AUTO_TEST_CASE(getNumtypeAsText_casef64) {
   uint8_t numtypeContent = f64;
   auto returnNumtype = antiwasm::parseNumType(numtypeContent);
 
-  displayNumtype(returnNumtype);
+  getNumtypeAsText(returnNumtype);
 }
 
-BOOST_AUTO_TEST_CASE(displayNumType_caseError) {
+BOOST_AUTO_TEST_CASE(getNumtypeAsText_caseError) {
   uint8_t numtypeContent = 0xCA; // Incorrect numtype
   auto returnNumtype = antiwasm::parseNumType(numtypeContent);
 
-  displayNumtype(returnNumtype);
+  auto str = getNumtypeAsText(returnNumtype);
 }
 
 BOOST_AUTO_TEST_SUITE_END() // numtype_test
